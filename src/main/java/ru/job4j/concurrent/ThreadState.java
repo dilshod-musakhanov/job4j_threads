634a859e99a5cbf94087b8b85f74d94d48a9cd3c
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-10-12 15:08:02 +0400

    Состояние нити. [#229175 #350976]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -12,7 +12,7 @@ public class ThreadState {
         System.out.println(second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName());
             System.out.println(second.getName());
         }
