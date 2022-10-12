e56ec84191d743769676280287bb691cfb19cf94
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-10-12 15:47:39 +0400

    Прерывание нити [#1019 #351014]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -3,12 +3,15 @@ package ru.job4j.concurrent;
 public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
+        String[] process = new String[]{"\\|", " |/"};
+        int count = 0;
         try {
             while (!Thread.currentThread().isInterrupted()) {
-                System.out.print("\r load: " + "\\|");
+                System.out.print("\r load: " + process[count++]);
                 Thread.sleep(500);
-                System.out.print("\r load: " + " |/");
-                Thread.sleep(500);
+                if (count == process.length) {
+                    count = 0;
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
