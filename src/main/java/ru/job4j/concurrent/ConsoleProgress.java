96d0f73ecef190bded584c84cce0c22c5d2cb70b
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-10-12 19:07:20 +0400

    Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -14,7 +14,7 @@ public class ConsoleProgress implements Runnable {
                 }
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 
