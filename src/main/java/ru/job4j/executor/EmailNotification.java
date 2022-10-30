4aa87c8454fd21133df38bb842beb33e17cef65d
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-10-30 15:15:31 +0400

    ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/executor/EmailNotification.java
@@ -0,0 +1,24 @@
+package ru.job4j.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = String.format("Notification %s to email %s", user.getUserName(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getUserName());
+            send(subject, body, user.getEmail());
+        });
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+}
